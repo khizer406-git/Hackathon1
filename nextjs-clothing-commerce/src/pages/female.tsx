1664ce1c2000b6d39958d7bca6f389d7915686d2
khizer406-git <khizerjilani@gmail.com>
2023-09-06 22:22:58 +0500

    .

--- nextjs-clothing-commerce/src/pages/female.tsx
+++ nextjs-clothing-commerce/src/pages/female.tsx
@@ -1,36 +1,32 @@
-'use client'
-
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
-import images from './data'
-import { useSelector} from 'react-redux';
-import { useState } from 'react';
+import images from './data';
+import { useSelector } from 'react-redux';
 import { useRouter } from 'next/router';
 
-const female = () => {
+const Female = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [length, setLength] = useState(0);
+  const [data, setData] = useState(images);
+  const search = useSelector((state:any) => state.search);
+  const router = useRouter();
 
-const [currentIndex, setCurrentIndex] = useState(0);
-const [length,setlength] = useState(0);
-const [data,setData] = useState(images);
-const search = useSelector((state:any) => state.search);
-const router = useRouter();
+  useEffect(() => {
+    console.log(search);
+    setData(images);
+    setData((prevData) => {
+      const newData = [...prevData];
+      if (search && prevData) {
+        return newData.filter((data) =>
+          data.Name.toLowerCase().includes(search)
+        );
+      }
+      return images;
+    });
+  }, [search]);
 
-useEffect(() => {
-  console.log(search);
-  setData(()=>images)
-  setData((prevData) => {
-    const newData = [...prevData]; 
-    if (search && prevData) {
-      return newData.filter((data) =>
-        data.Name.toLowerCase().includes(search)
-      );
-    }
-    return images; 
-  });
-}, [search]);
-
-  const navigateToDestination = (name:string,src:string,price:number) => {
+  const navigateToDestination = (name:any, src:any, price:any) => {
     router.push({
       pathname: '/ViewProduct',
       query: {
@@ -41,57 +37,77 @@ useEffect(() => {
     });
   };
 
-useEffect(()=>{
-  setlength(data.filter(image => image.gender === 'female').length);
-},[data])
+  useEffect(() => {
+    setLength(data.filter((image) => image.gender === 'female').length);
+  }, [data]);
 
-const prevImage = () => {
-  setCurrentIndex((prevIndex) => Math.max(prevIndex - 4, 0));
-};
+  const prevImage = () => {
+    setCurrentIndex((prevIndex) => Math.max(prevIndex - 4, 0));
+  };
 
-const nextImage = () => {
-  setCurrentIndex((prevIndex) => Math.min(prevIndex + 4, length - 4));
-};
-
+  const nextImage = () => {
+    setCurrentIndex((prevIndex) =>
+      Math.min(prevIndex + 4, length - 4)
+    );
+  };
 
   return (
-    <div className="my-14 mx-24" >
+    <div className="my-14 mx-24">
       <Navbar />
-        <div className="flex flex-col items-center justify-center my-20 ">
+      <div className="flex flex-col items-center justify-center my-20 ">
         <div className="flex items-center space-x-4">
-        <button
+          <button
             onClick={prevImage}
             disabled={currentIndex === 0}
-            className={`px-4 py-2 mr-2 ${currentIndex === 0 ? 'bg-gray-300 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-700 text-white'} rounded`}
-            >
+            className={`px-4 py-2 mr-2 ${
+              currentIndex === 0
+                ? 'bg-gray-300 cursor-not-allowed'
+                : 'bg-blue-500 hover:bg-blue-700 text-white'
+            } rounded`}
+          >
             &#8249;
-            </button>
-            {data.filter(image => image.gender === 'female').slice(currentIndex, currentIndex + 4).map((image, index) => (
-            <div className="w-full h-full transition-transform transform-gpu hover:scale-110" key={index}
-            onClick={()=>{navigateToDestination(image.Name,image.src,image.Price)}}>
-            <img
+          </button>
+          {data
+            .filter((image) => image.gender === 'female')
+            .slice(currentIndex, currentIndex + 4)
+            .map((image, index) => (
+              <div
+                className="w-full h-full transition-transform transform-gpu hover:scale-110"
                 key={index}
-                src={image.src}
-                alt={`Image ${currentIndex + index + 1}`}
-            />
-            <div className='font-bold'>{image.Name}</div>
-            <div className='font-bold'>${image.Price}</div>
-            </div>
+                onClick={() => {
+                  navigateToDestination(
+                    image.Name,
+                    image.src,
+                    image.Price
+                  );
+                }}
+              >
+                <img
+                  key={index}
+                  src={image.src}
+                  alt={`Image ${currentIndex + index + 1}`}
+                />
+                <div className="font-bold">{image.Name}</div>
+                <div className="font-bold">${image.Price}</div>
+              </div>
             ))}
-            <button
+          <button
             onClick={nextImage}
             disabled={currentIndex + 4 >= length}
-            className={`px-4 py-2 ${currentIndex + 4 >= length ? 'bg-gray-300 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-700 text-white'} rounded`}
-            >
+            className={`px-4 py-2 ${
+              currentIndex + 4 >= length
+                ? 'bg-gray-300 cursor-not-allowed'
+                : 'bg-blue-500 hover:bg-blue-700 text-white'
+            } rounded`}
+          >
             &#8250;
-            </button>
+          </button>
         </div>
-        <div className="flex mt-4">
-        </div>
-        </div>
+        <div className="flex mt-4"></div>
+      </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default female;
+export default Female;
