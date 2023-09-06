9d0e9dcba4bd80a1ba600bf8b4ece5a7d1f486c7
khizer406-git <khizerjilani@gmail.com>
2023-09-07 01:12:07 +0500

    .

--- nextjs-clothing-commerce/src/pages/_app.js
+++ nextjs-clothing-commerce/src/pages/_app.js
@@ -9,10 +9,9 @@ import { Toaster } from 'react-hot-toast';
 import { ClerkProvider } from '@clerk/nextjs';
 
 function MyApp({ Component, pageProps }) {
-  console.log(process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY)
   return (
     <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      frontendApi={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
     >
       <Provider store={store}>
         <Toaster />      
